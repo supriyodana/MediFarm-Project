84067cc4515ebbfa70563a5cc8574efc073943f5
Supriyo <supriyodana@gmail.com>
2025-12-20 16:42:28 +0530

    .

--- MediFarm-Project-Backend/src/models/bloodTestBooking.js
+++ MediFarm-Project-Backend/src/models/bloodTestBooking.js
@@ -1,10 +1,10 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const bloodTestSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  testType: String,
-  appointmentDate: Date,
-  status: { type: String, default: "booked" }
-}, { timestamps: true });
+// const bloodTestSchema = new mongoose.Schema({
+//   user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+//   testType: String,
+//   appointmentDate: Date,
+//   status: { type: String, default: "booked" }
+// }, { timestamps: true });
 
-module.exports = mongoose.model("BloodTestBooking", bloodTestSchema);
+// module.exports = mongoose.model("BloodTestBooking", bloodTestSchema);
